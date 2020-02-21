21ddfaad0e3229c3f18161b041ae48d743f84119
azhar-torinit <azhar@torinit.ca>
2020-02-21 14:42:14 +0530

    welcome message on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Hello Laravel
+                   Welcome to Website
                 </div>
 
                 <div class="links">
